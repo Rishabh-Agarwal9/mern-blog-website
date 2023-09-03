d2433abf0292fcce8b459ed2548b9bb4fdb67ec3
Rishabh <rishabh.9agarwal@gmail.com>
2023-09-04 03:57:57 +0530

    fixing login

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,20 @@ import {Routes, Route} from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import { UserContextProvider } from './UserContext';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path ="/" element={<Layout/>}>
-        <Route index element={<IndexPage/>}/>
-        <Route path ='/login' element={<LoginPage/>}/>
-        <Route path ='/register' element={<RegisterPage/>}/>
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path ="/" element={<Layout/>}>
+          <Route index element={<IndexPage/>}/>
+          <Route path ='/login' element={<LoginPage/>}/>
+          <Route path ='/register' element={<RegisterPage/>}/>
+        </Route>
+      </Routes>
+    </UserContextProvider>
+    
   )
 }
 
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
+import React, { useContext } from 'react'
+import { useEffect } from 'react'
 import {Link} from "react-router-dom"
+import { UserContext } from './UserContext'
 
 const Header = () => {
-  const [username, setUsername] = useState(null);
-
+  const {setUserInfo,userInfo}= useContext(UserContext);
   useEffect(() => {
     fetch('http://localhost:4000/profile', {
       credentials: 'include',
     }).then(response => {
       response.json().then(userInfo => {
-        setUsername(userInfo);
+        setUserInfo(userInfo);
       });
     });
   }, []);
@@ -20,9 +20,9 @@ const Header = () => {
       credentials: 'include',
       method: 'POST',
     });
-    setUsername(null);
+    setUserInfo(null);
   }
-  
+  const username =userInfo?.username;
   return (
     <header>
         <Link to="/"  className="logo">MYblog</Link>
