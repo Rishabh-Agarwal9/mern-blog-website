8743768b9ea7694423487e3a555308fb31daf860
Rishabh <rishabh.9agarwal@gmail.com>
2023-09-04 03:13:19 +0530

    login functionallity

--- api/index.js
+++ api/index.js
@@ -1,46 +1,67 @@
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const User = require('./models/User');
-const bcrypt = require('bcryptjs'); 
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
 const app = express();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const salt = bcrypt.genSaltSync(10);
+const secret = "siudbvsw9843ofna29";
 
-app.use(cors()); 
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
+app.use(cookieParser());
 mongoose.connect(process.env.MONGO_TOKEN);
-app.post('/register',async (req,res)=>{
-    const {username, password}=req.body;
-    try{
-        const UserDoc= await User.create({
-            username,
-            password: bcrypt.hashSync(password,salt)
-        });    
-        res.json(UserDoc);
-    } catch(e){
-        res.status(400).json(e);
-    }
-    
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
 });
 
-app.post('/login', async (req,res)=>{
-    const{username, password} = req.body;
-    const userDoc = await User.findOne({username});
-    const passOk = bcrypt.compareSync(password, userDoc.password);
-    if(passOk){
-        //logged in
-    }
-    else{
-        res.status(400).json('Wrong credentials');
-    }
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    // logged in
+    //converting(encrypting) username and user id into a token and storing it in a cookie
 
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      //reading token from cookie
 
+      res.cookie("token", token).json({
+        id: userDoc._id,
+        username,
+      });
+    });
+  } else {
+    res.status(400).json("wrong credentials");
+  }
+});
 
-})
-  
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
+});
 
 app.listen(4000);
-
-
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,13 +1,44 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 import {Link} from "react-router-dom"
 
 const Header = () => {
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUsername(userInfo);
+      });
+    });
+  }, []);
+
+  function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUsername(null);
+  }
+  
   return (
     <header>
         <Link to="/"  className="logo">MYblog</Link>
         <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
+          {username &&(
+            <>
+            <Link to = '/create'>Create new post</Link>
+            <a onClick={logout}>Logout</a>
+            </>
+          )}
+          {!username &&(
+            <>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+            </>
+          )}
         </nav>
       </header>
   )
