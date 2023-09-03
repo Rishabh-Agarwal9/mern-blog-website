32531a8b51670ed31bb9dfc0b2e1d911fd3c69c1
Rishabh <rishabh.9agarwal@gmail.com>
2023-09-03 20:46:05 +0530

    changed stuff

--- api/index.js
+++ api/index.js
@@ -1,17 +1,16 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs'); 
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
 
-app.use(cors());
+app.use(cors()); 
 app.use(express.json());
-mongoose.connect('mongodb+srv://blog:3m48zTpYGlLsxhwx@cluster0.zpljtxq.mongodb.net/?retryWrites=true&w=majority');
-
-
+mongoose.connect(process.env.MONGO_TOKEN);
 app.post('/register',async (req,res)=>{
     const {username, password}=req.body;
     try{
@@ -25,6 +24,21 @@ app.post('/register',async (req,res)=>{
     }
     
 });
+
+app.post('/login', async (req,res)=>{
+    const{username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if(passOk){
+        //logged in
+    }
+    else{
+        res.status(400).json('Wrong credentials');
+    }
+
+
+
+})
   
 
 app.listen(4000);
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import { useState } from 'react';
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  async function login(ev){
+    ev.preventDefault();
+    await fetch('http://localhost:4000/login',{
+      method: 'POST',
+      body: JSON.stringify({username,password}),
+      headers:{'Content-Type': 'application/json'}
+
+    });
+  }
+
   return (
-    <form className='login'>
+    <form className='login' onSubmit={login}>
         <h1>Login</h1>
-        <input type="text" placeholder='Username' />
-        <input type="password" placeholder='Password'/>
+        <input 
+          type="text"
+          values = {username}
+          onChange={e=>{
+            setUsername(e.target.value)
+          }}
+          placeholder='Username' />
+
+        <input
+          type="password"
+          values = {password}
+          onChange={e=>{
+            setPassword(e.target.value)
+          }}
+          placeholder='Password'
+          />
         <button>Login</button>
     </form>
   )
